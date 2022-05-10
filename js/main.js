6fe5e7c1c986f2ec63815ef9d2dcaa251e19ceed
Jeremy Pena <5196790+jerpena@users.noreply.github.com>
2022-05-09 23:35:16 -0500

    update api url

--- js/main.js
+++ js/main.js
@@ -1,21 +1,13 @@
+const foursquareUrl = 'https://node-api-router.herokuapp.com/places?near=';
+const weatherUrl = 'https://node-api-router.herokuapp.com/weather';
+const input = document.getElementById('city');
+const submit = document.getElementById('button');
+const destination = document.getElementById('destination');
+const container = document.querySelector('.container');
+const placeDivs = [document.getElementById("place1"), document.getElementById("place2"), document.getElementById("place3")];
+const weatherDiv = document.getElementById("weather1");
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-// Import API Keys
-import { foursquareKey, openWeatherKey } from './config.js'
-
-// Foursquare API Info
-const url = 'https://api.foursquare.com/v3/places/search?near=';
-const options = {
-    method: 'GET',
-    headers: {
-        Accept: 'application/json',
-        Authorization: foursquareKey
-    }
-};
-
-// OpenWeather Info
-const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
-
-// HTML Creation
 const createPlaceHTML = (name, location, iconSource) => {
     return `<h3>${name}</h3>
         <img class="placeimage" src="${iconSource}"/>
@@ -33,26 +25,13 @@ const createWeatherHTML = (currentDay) => {
         <img src="https://openweathermap.org/img/wn/${currentDay.weather[0].icon}@2x.png" class="weathericon">`;
 }
 
-// Convert Kelvin to Fahrenheight
 const kelvinToFahrenheit = k => ((k - 273.15) * 9 / 5 + 32).toFixed(0);
 
-
-
-// Page Elements
-const input = document.getElementById('city');
-const submit = document.getElementById('button');
-const destination = document.getElementById('destination');
-const container = document.querySelector('.container');
-const placeDivs = [document.getElementById("place1"), document.getElementById("place2"), document.getElementById("place3")];
-const weatherDiv = document.getElementById("weather1");
-const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-// Fetch from APIs
 const getPlaces = async () => {
     const city = input.value;
-    const urlToFetch = `${url}${city}&limit=10`
+    const urlToFetch = `${foursquareUrl}${city}`
     try {
-        const response = await fetch(urlToFetch, options);
+        const response = await fetch(urlToFetch);
         if (response.ok) {
             const jsonResponse = await response.json();
             const places = jsonResponse.results
@@ -64,7 +43,7 @@ const getPlaces = async () => {
 };
 
 const getForecast = async () => {
-    const urlToFetch = `${weatherUrl}?q=${input.value}&APPID=${openWeatherKey}`
+    const urlToFetch = `${weatherUrl}?q=${input.value}`
     try {
         const response = await fetch(urlToFetch)
         if (response.ok) {
@@ -77,10 +56,8 @@ const getForecast = async () => {
 };
 
 
-// Render functions
 const renderPlaces = (places) => {
     placeDivs.forEach((location, index) => {
-        // Add your code here:
         const place = places[index]
         const placeIcon = place.categories[0].icon
         const placeImgSrc = `${placeIcon.prefix}bg_64${placeIcon.suffix}`
@@ -95,7 +72,6 @@ const renderForecast = (forecast) => {
     weatherDiv.innerHTML = weatherContent;
 };
 
-// Event listeners
 submit.addEventListener('click', event => {
     event.preventDefault()
     placeDivs.forEach(place => place.replaceChildren());
